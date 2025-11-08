dafb7c6043264d44cedc10d82d04231efa149f56
Le Huynh Thai <thailh0902@gmail.com>
2025-11-07 20:35:14 -0800

    up

---
+++ HuongDanNauAn/Controller/HomeViewController.swift
@@ -0,0 +1,59 @@
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNavigationBar()
+    }
+    
+    func setupNavigationBar() {
+        // StackView
+        let leftStackView = UIStackView()
+        leftStackView.axis = .horizontal
+        leftStackView.spacing = 8
+        leftStackView.alignment = .center
+        
+        // Logo
+        let logoImageView = UIImageView(image: UIImage(named: "chef"))
+        logoImageView.contentMode = .scaleAspectFit
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Title
+        let titleLabel = UILabel()
+        titleLabel.text = "CookEase"
+        titleLabel.font = UIFont.systemFont(ofSize: 22, weight: .semibold)
+        titleLabel.textColor = .label
+        
+        leftStackView.addArrangedSubview(logoImageView)
+        leftStackView.addArrangedSubview(titleLabel)
+        
+        // Constraints cho logo
+        NSLayoutConstraint.activate([
+            logoImageView.widthAnchor.constraint(equalToConstant: 32),
+            logoImageView.heightAnchor.constraint(equalToConstant: 32)
+        ])
+        
+        let leftBarButton = UIBarButtonItem(customView: leftStackView)
+        navigationItem.leftBarButtonItem = leftBarButton
+        
+        // AVATAR
+        let profileButton = UIButton(type: .custom)
+        profileButton.frame = CGRect(x: 0, y: 0, width: 40, height: 40)
+        profileButton.layer.cornerRadius = 20
+        profileButton.clipsToBounds = true
+        
+        if let avatarImage = UIImage(named: "user_avatar") {
+        profileButton.setImage(avatarImage, for: .normal)
+        profileButton.imageView?.contentMode = .scaleAspectFill
+        } else {
+            profileButton.backgroundColor = .systemGray4
+        }
+        
+        profileButton.layer.borderWidth = 0.5
+        profileButton.layer.borderColor = UIColor.systemGray5.cgColor
+        
+        let profileBarButton = UIBarButtonItem(customView: profileButton)
+        navigationItem.rightBarButtonItem = profileBarButton
+    }
+}
