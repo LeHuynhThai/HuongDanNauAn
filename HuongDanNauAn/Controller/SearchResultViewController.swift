670fb32caf846e449873b12f156d39a8d38fd279
quandang08 <22211TT1369@mail.tdc.edu.vn>
2025-11-18 18:35:43 -0800

    Search Result Reciept UI

--- HuongDanNauAn/Controller/SearchResultViewController.swift
+++ HuongDanNauAn/Controller/SearchResultViewController.swift
@@ -18,25 +18,28 @@ class SearchResultViewController: UIViewController, UICollectionViewDelegate, UI
         collectionView.delegate = self
         collectionView.dataSource = self
         collectionView.collectionViewLayout = UICollectionViewFlowLayout()
+
+        // Fake Data
+        searchResults = ["Món 1", "Món 2", "Món 3", "Món 4", "Món 5", "Món 6"]
+
+        collectionView.reloadData()
     }
 
-    // MARK: - UICollectionViewDataSource
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return searchResults.count
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! RecipeCell
+        
         cell.RecipeName.text = searchResults[indexPath.row]
-        cell.RecipeImageView.image = UIImage(named: searchResults[indexPath.row])
+        cell.RecipeImageView.image = UIImage(named: "1") // ảnh giả
+        
         return cell
     }
 
-    // MARK: - UICollectionViewDelegateFlowLayout
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let size = (collectionView.frame.width - 10) / 2
         return CGSize(width: size, height: size)
     }
-    
-    
 }
